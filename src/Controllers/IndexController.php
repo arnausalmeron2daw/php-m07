830de5e12545143134ae8cf9230a3d3c4ee238db
arnausalmeron2daw <asalmeron2daw@gmail.com>
2023-12-05 18:13:29 +0100

    Segundo comit

---
+++ src/Controllers/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+    namespace App\Controllers;
+    use App\Request;
+    use App\Controller;
+    use App\View;
+    class IndexController extends Controller {
+        function __construct($session,$request)
+        {
+            parent::__construct($session,$request); 
+        }
+        function index(){
+            $data=[
+                'title'=>'PHP2324',
+                'user'=>'Linus'
+            ];
+
+            $users=Registry::get('database')->selectAll('users');
+
+            echo View::render('home',$data);
+            
+            
+        }
+        
+    }
